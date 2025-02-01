f1bf614bf76a7d6788b099a0e19492997edfe6ad
thoufiq135 <shaikno150@gmail.com>
2025-02-01 17:27:27 +0530

    changed

--- formfront/src/form.jsx
+++ formfront/src/form.jsx
@@ -6,13 +6,21 @@ function Form(){
     const [number,setnumber]=useState(null)
     const [warning,setwarning]=useState(false)
     const [final,setfinal]=useState(null)
+    const[loder,setloader]=useState(false)
+    console.log(loder)
 
     async function setdata(e) {
         e.preventDefault(); 
+        
 
      if(email&&name&&number){
-        setwarning(false)
+       
+        
         console.log(`name=${name} email=${email} number${number}`)
+        setloader(true)
+        console.log("this",loder)
+        setwarning(true)
+        console.log("that",loder)
         const response =await fetch('https://formserver.vercel.app/add',{
             method:'POST',
             body:JSON.stringify({
@@ -27,17 +35,25 @@ function Form(){
         {if(data){
             console.log(data)
             setfinal(data)
+       
         }else{setfinal("Registered")}})).catch((e)=>{
             setfinal("Registered")
-        } )     
+        } ).finally(setloader(false))  
      }else{
         setwarning(true)
+        setloader(false)
      }
     }
 
     return(
         <>
-        <form>
+        {final==="Registered"?(<>
+        <h1 id="sel">Registered</h1>
+        <div id="data"><h2 id="nam">Name         : {name}</h2>
+        <h2 id="mail">E-mail      : {email}</h2>
+        <h2 id="pho">Pho-Number   : {number}</h2></div>
+        </>):(<>
+            <form> 
             <div id="par">
            <div id="namediv">
            <label className="name" for="name">Enter your name</label ><br/>
@@ -55,7 +71,9 @@ function Form(){
                 <button onClick={setdata}>Submit</button>
             </div>
         </form>
-       {final?<h2>{final}</h2>:""}
+        {final?<h2 id="already">Already Registered</h2>:""}
+        {loder?(<><h3>Loading...</h3></>):""}
+        </>)}
         </>
     )
 }
